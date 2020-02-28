aa4a99d3c76936c7da5e5b89d7d76e0d995be98e
dannysofftie <dankim761@gmail.com>
2020-02-28 11:12:18 +0300

    Update to electron 8

--- src/keymap.js
+++ src/keymap.js
@@ -1,9 +1,9 @@
-'use strict';
-const {globalShortcut} = require('electron');
-const {shortcutKeys} = require('./config');
-const win = require('./win');
+"use strict";
+const { globalShortcut } = require("electron");
+const { shortcutKeys } = require("./config");
+const win = require("./win");
 
-const {log} = console;
+const { log } = console;
 
 class Keymap {
   setAcc(custom, predefined) {
@@ -16,26 +16,32 @@ class Keymap {
 
   registerGlobal() {
     const toggleAo = globalShortcut.register(
-      this.setAcc('global-toggle-window', 'CmdorCtrl+Alt+A'), () => {
+      this.setAcc("global-toggle-window", "CmdorCtrl+Alt+A"),
+      () => {
         win.toggle();
-      });
+      }
+    );
 
     const searchTodo = globalShortcut.register(
-      this.setAcc('global-search-todo', 'CmdorCtrl+Alt+F'), () => {
+      this.setAcc("global-search-todo", "CmdorCtrl+Alt+F"),
+      () => {
         win.appear();
-        win.activate('search');
-      });
+        win.activate("search");
+      }
+    );
 
     const createTodo = globalShortcut.register(
-      this.setAcc('global-create-todo', 'CmdorCtrl+Alt+C'), () => {
+      this.setAcc("global-create-todo", "CmdorCtrl+Alt+C"),
+      () => {
         win.appear();
-        win.activate('new-todo');
-      });
+        win.activate("new-todo");
+      }
+    );
 
     if (toggleAo && searchTodo && createTodo) {
-      log('Successfully registered global shortcut keys');
+      log("Successfully registered global shortcut keys");
     } else {
-      log('Global shortcut keys registration failed');
+      log("Global shortcut keys registration failed");
     }
   }
 }
